85dca765fafedb113bb733e38cdd12490141b126
Casandra <casandragrullon@pursuit.org>
2019-10-17 10:45:05 -0400

    question 3 with alex

--- EnumerationsLab.playground/Contents.swift
+++ EnumerationsLab.playground/Contents.swift
@@ -97,21 +97,37 @@ enum Coin: Int {
 }
 
 // Your function here
+func getTotalValue(from tuples: [(count: Int, coinType: Coin)]) -> Int {
+    var totalNumberOfCents = 0
+    for tuple in tuples {
+        switch tuple.coinType {
+        case .penny :
+            totalNumberOfCents += Coin.penny.rawValue * tuple.count
+        case .nickle :
+            totalNumberOfCents += Coin.nickle.rawValue * tuple.count
+        case .dime :
+            totalNumberOfCents += Coin.dime.rawValue * tuple.count
+        case .quarter :
+            totalNumberOfCents += Coin.quarter.rawValue * tuple.count
+        }
+    }
+    return totalNumberOfCents
+}
 
 // Uncomment the lines below to test your solution
 
-//let coinArr: [(Int, Coin)] = [
-//    (10, .penny),
-//    (15, .nickle),
-//    (3, .quarter),
-//    (20, .penny),
-//    (3, .dime),
-//    (7, .quarter)
-//]
-//
-//let expectedTotal = 385
-//let total = getTotalValue(from: coinArr)
-//assert(total == expectedTotal, "Was expecting \(expectedTotal), but got \(total)")
+let coinArr: [(Int, Coin)] = [
+    (10, .penny),
+    (15, .nickle),
+    (3, .quarter),
+    (20, .penny),
+    (3, .dime),
+    (7, .quarter)
+]
+
+let expectedTotal = 385
+let total = getTotalValue(from: coinArr)
+assert(total == expectedTotal, "Was expecting \(expectedTotal), but got \(total)")
 
 // Question Four
 
